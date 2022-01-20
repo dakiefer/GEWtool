ec951a2df7fa3f9f74b41d425f35faf3d157eded
Daniel Kiefer <daniel.kiefer@fau.de>
2022-01-20 23:08:40 +0100

    Material: convert Voigt notated stiffenss matrix to 4th order tensor.

---
+++ Material/voigt2tensor.m
@@ -0,0 +1,22 @@
+function c = voigt2tensor(C)
+% VOIGT2TENSOR returns the 4th order stiffness tensor c corresponding to the Voigt
+% notated matrix C.
+%
+% 2022 - Daniel Kiefer
+% Institut Langevin, Paris, France
+
+validateattributes(C,{'numeric'},{'size',[6 6]});
+if ~issymmetric(C), error('Voigt matrix C must be symmetric.'); end
+warning('untested')
+c = zeros(3, 3, 3, 3);
+IJ = [ {1}, {1}; {2}, {2}; {3}, {3}; {2}, {3}; {3}, {1}; {1}, {2}]; % voigt -> tensor indices
+for i = 1:6
+    for j = 1:6
+        c(IJ{i,:}, IJ{j,:}) = C(i, j);
+        c(IJ{i,end:-1:1}, IJ{j,:}) = C(i, j);
+        c(IJ{i,:}, IJ{j,end:-1:1}) = C(i, j);
+        c(IJ{i,end:-1:1}, IJ{j,end:-1:1}) = C(i, j);
+    end
+end
+
+end
