91408e3f561eceefd3cc8ff8d1af2aef9263f0fa
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-04 16:48:51 +0200

    MaterialIsotropic: specialization for isotropic material

---
+++ Material/MaterialIsotropic.m
@@ -0,0 +1,25 @@
+classdef MaterialIsotropic < Material
+    %MaterialIsotropic specialization for isotropic materials.
+
+    properties (Dependent)
+        lambda      % first Lamé parameter
+        mu          % second Lamé parameter
+        E           % Young's modulus
+        nu          % Poisson's ratio
+    end
+
+    methods
+        function lambda = get.lambda(obj)
+            lambda = obj.C(1,2);
+        end
+        function mu = get.mu(obj)
+            mu = obj.C(4,4);
+        end
+        function E = get.E(obj)
+            E = obj.mu*(3*obj.lambda + 2*obj.mu)/(obj.lambda + obj.mu);
+        end
+        function nu = get.nu(obj)
+            nu = obj.lambda/(2*(obj.lambda + obj.mu));
+        end
+    end
+end
