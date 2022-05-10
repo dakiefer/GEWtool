9240de9043a2325eef01613185e57410b7d9e123
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-10 18:29:20 +0200

    test added: Lamb waves in a double layered plate (twice the same material compared to single layer calculation)

---
+++ tests/plate_doubleLayer_Lamb.m
@@ -0,0 +1,26 @@
+%% compute guided ultrasonic waves in a layered plate
+% 
+% 2022 Daniel Kiefer
+% Institut Langevin, Paris, France
+% 
+
+steel = Material('steel');
+chrom = Material('chromium');
+hs = [0.71e-3, 0.29e-3]; % thickness list
+Ns = [15, 12]; % number of discretization points
+k = linspace(1e-2, 12, 200)/(sum(hs)); % wavenumbers to solve for
+
+%% one layer:
+plate = Plate(steel, sum(hs), sum(Ns));
+guw = plate.Lamb;
+dat = computeW(guw, k); 
+figure, plot(dat.k(:), dat.w(:)/2/pi, 'gx'); ylim([0, 6e3]/(sum(hs)));
+xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
+
+%% two layers of same material and same total thickness as one layer:
+plate = Plate([steel, steel], hs, Ns);
+guw = plate.Lamb;
+dat = computeW(guw, k); 
+hold on, plot(dat.k(:), dat.w(:)/2/pi, 'k.');  ylim([0, 6e3]/(sum(hs)));
+xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
+legend({'single', 'two lay.'}, 'Location','southeast')
