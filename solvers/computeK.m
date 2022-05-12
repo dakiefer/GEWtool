851e5beefc4705fb210ef75a4214e6dff1e6f6f0
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-12 16:37:01 +0200

    computeK(): support for linear EVP

--- solvers/computeK.m
+++ solvers/computeK.m
@@ -1,15 +1,25 @@
 function dat = computeK(guw, w, nModes)
-    if nargin < 3, nModes = 2*size(guw.op.M,1); end
     if ~isvector(w), error('Angular frequencies should be a [Nx1] array.'); end
     w = w(:); % column vector
     wh = w*guw.np.h0;
     M = guw.op.M; L0 = guw.op.L0; L1 = guw.op.L1; L2 = guw.op.L2;
+    if nargin < 3
+        if isequal(L2, zeros(size(L2)))
+            nModes = size(guw.op.M,1);
+        else
+            nModes = 2*size(guw.op.M,1);
+        end
+    end
     tic 
     kh = nan(length(wh), nModes);
     u = nan(length(wh), nModes, size(M,1));
     for ii = 1:length(wh)
         whn = wh(ii)/guw.np.fh0; % current frequency-thickness (normalized)
-        [ui, ikhi] = polyeig(L0 + whn^2*M, L1, L2);
+        if isequal(L2, zeros(size(L2)))
+            [ui, ikhi] = polyeig(L0 + whn^2*M, L1);
+        else
+            [ui, ikhi] = polyeig(L0 + whn^2*M, L1, L2);
+        end
         spurious = abs(ikhi)<=1e-8; ikhi(spurious) = nan; ui(:,spurious) = nan;
         [khi, ind] = sort(-1i*ikhi);
         ui = ui(:,ind); % sort
