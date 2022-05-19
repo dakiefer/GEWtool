75bffe4184e5cb54c79691c4bed5232b75227b50
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-19 20:22:49 +0200

    solvers: do not output computing time. simplifications and comments added.

--- solvers/computeK.m
+++ solvers/computeK.m
@@ -4,38 +4,33 @@ function dat = computeK(guw, w, nModes)
     wh = w*guw.np.h0;
     M = guw.op.M; L0 = guw.op.L0; L1 = guw.op.L1; L2 = guw.op.L2;
     if nargin < 3
-        if isequal(L2, zeros(size(L2)))
-            nModes = size(guw.op.M,1);
-        else
-            nModes = size(guw.op.M,1);
-        end
+        nModes = size(guw.op.M,1);  % for now we dont distinguish between linearized and quadratic EVP
     end
-    tic 
     kh = nan(length(wh), nModes);
     u = nan(length(wh), nModes, size(M,1));
     for ii = 1:length(wh)
         whn = wh(ii)/guw.np.fh0; % current frequency-thickness (normalized)
-        if isequal(L2, zeros(size(L2)))
-            [ui, ikhi] = polyeig(L0 + whn^2*M, L1);
-        elseif isempty(L1)
-            [ui, ikhi2] = polyeig(L0 + whn^2*M, L2);
+        if isequal(L2, zeros(size(L2))) % is linearized as [(ik) L1 + L0 + w^2 M]*u = 0
+            [ui, ikhi] = polyeig(L0 + whn^2*M, L1); 
+        elseif isempty(L1) % is linearized as [(ik)^2 L2 + L0 + w^2 M]*u = 0
+            [ui, ikhi2] = polyeig(L0 + whn^2*M, L2); % calculate (ikh)^2
             ikhi = sqrt(ikhi2);
             N = guw.geom.N; dofy = N+1:2*N;
-            ui(dofy,:) = ikhi.'.*ui(dofy,:);
-        else
+            ui(dofy,:) = ikhi.'.*ui(dofy,:); % eig.vec. was [ux, 1i*k*uy]
+        else % quadratic EVP: [(ik)^2 L2 + (ik) L1 + L0 + w^2 M]*u = 0
             [ui, ikhi] = polyeig(L0 + whn^2*M, L1, L2);
         end
-        spurious = abs(ikhi)<=1e-8; ikhi(spurious) = nan; ui(:,spurious) = nan;
+        spurious = isinf(ikhi) | isnan(ikhi); ikhi(spurious) = []; ui(:,spurious) = [];
         [khi, ind] = sort(-1i*ikhi);
         ui = ui(:,ind); % sort
+%         [~, ind] = sort(real(khi));
+%         khi = khi(ind); ui = ui(:,ind); % sort
         u(ii,:,:) = ui(:,1:nModes).'; % save
         kh(ii, :) = khi(1:nModes);
     end
-    chron = toc; 
-    fprintf('nF: %d, nK: %d, elapsed time: %g, time per point: %g. ms\n', size(kh, 1), size(kh, 2), chron, chron/length(kh(:))*1e3);
     dat.k = kh/guw.np.h0;
     dat.w = w.*ones(size(kh));
-    dat.u = cell(guw.geom.nLay, 1);
+    dat.u = cell(guw.geom.nLay, 1); % initialize
     for i = 1:guw.geom.nLay
         ulay = u(:,:,guw.geom.gdofOfLay{i});
         dat.u{i} = reshape(ulay, [size(kh), guw.geom.N(i), guw.geom.Nudof(i)]);
