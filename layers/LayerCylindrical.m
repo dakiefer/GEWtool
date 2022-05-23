9130415889da2d0d7abcdd4125c6b422963b553e
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-23 19:31:51 +0200

    LayerCylindrical: transitioned to SEM (spectral element method)

--- layers/LayerCylindrical.m
+++ layers/LayerCylindrical.m
@@ -1,4 +1,9 @@
 classdef LayerCylindrical < Layer
+    properties 
+        PPr     % integral of weighted product matrix of ansatz functions ∫1/r*P*P dr
+        PPr2    % integral of weighted product matrix of ansatz functions ∫1/r^2*P*P dr 
+        PPdr    % integral of weighted product matrix of ansatz functions ∫1/r*P*P' dr 
+    end
     properties (Dependent)
         r
     end
@@ -7,6 +12,14 @@ classdef LayerCylindrical < Layer
         function obj = LayerCylindrical(mat, rs, N)
             % LayerCylindrical: constructor
             obj = obj@Layer(mat, rs, N);
+
+            % % element matrices specific to cylindrical coordinates:
+            [yi, wi] = Layer.nodes(N);  % nodal coordinates and integration weights
+            [P, Pd] = Layer.basis(yi);  % polynomial basis
+            rn = obj.r/obj.h; % radial coordinates normalized to thickness
+            obj.PPr = LayerCylindrical.elemPPr(P, wi, rn);          % ∫1/r*P*P dr
+            obj.PPr2 = LayerCylindrical.elemPPr2(P, wi, rn);        % ∫1/r^2*P*P dr 
+            obj.PPdr = LayerCylindrical.elemPPdr(P, Pd, wi, rn);    % ∫1/r*P*P' dr 
         end
 
         function r = get.r(obj)
@@ -20,25 +33,30 @@ classdef LayerCylindrical < Layer
             cxx = squeeze(cn(1,udof,udof,1));
             crr = squeeze(cn(2,udof,udof,2));
             cpp = squeeze(cn(3,udof,udof,3));
-            Crp = squeeze(cn(2,udof,udof,3)) + squeeze(cn(3,udof,udof,2));
-            Cxp = squeeze(cn(1,udof,udof,3)) + squeeze(cn(3,udof,udof,1));
-            Crx = squeeze(cn(2,udof,udof,1)) + squeeze(cn(1,udof,udof,2));
+            crp = squeeze(cn(2,udof,udof,3));
+            cpr = squeeze(cn(3,udof,udof,2));
+            cxp = squeeze(cn(1,udof,udof,3));
+            cpx = squeeze(cn(3,udof,udof,1));
+            crx = squeeze(cn(2,udof,udof,1));
+            cxr = squeeze(cn(1,udof,udof,2));
             % differetiation in curvilinear coordinate system:
             A = [0, 0, 0; 0, 0, -1; 0, 1, 0]; A = squeeze(A(udof, udof));
             B = [0, 0, 0; 0, 1,  0; 0, 0, 1]; B = squeeze(B(udof, udof));
-            % differentiation matrices on normalized domain:
-            Dr1 = obj.D1; Dr2 = obj.D2; r = obj.r;
-            Id = eye(size(Dr1)); % identity matrix for discretization
-            % operators:
-            L2 = kron(cxx, Id); 
-            L1 = kron(Cxp*A, diag(1./r)) + kron(Crx, Dr1) + 1i*n*kron(Cxp, diag(1./r));
-            L0 = kron(crr, Dr2) + kron(Crp*A, diag(1./r)*Dr1 - diag(1./r.^2)) ...
-                + kron(cpp, diag(1./r)*Dr1) - kron(cpp*B, diag(1./r.^2)) ...
-                + 1i*n*(kron(Crp, diag(1./r)*Dr1 - diag(1./r.^2)) + kron(2*cpp*A, diag(1./r.^2))) ...
-                + (1i*n).^2*(kron(cpp, diag(1./r.^2)));
+            I = eye(size(A));
+            
+            % element stiffness:
+            K2 = kron(cxx, obj.PP);
+            K1 = kron(cxr, obj.PPd) + kron( (cxp + cpx)*(1i*n*I + A) , obj.PPr);
+            k0PPdr = cpp + cpr*(1i*n*I + A); % first term in K0
+            k0PPr2 = -cpp*B - (crp + cpr)*A + (1i*n)*(cpp*2*A - (crp + cpr)*I) + (1i*n)^2*cpp; % second term in K0
+            K0 = kron(k0PPdr, obj.PPdr) + kron(k0PPr2, obj.PPr2);
+            % element flux:
+            [G0, G1] = obj.tractionOp(udof, n);
+            % combine to polynomial of (ik):
+            L2 = K2; L1 = K1 + G1; L0 = K0 + G0;
         end
 
-        function [B0, B1] = tractionOp(obj, udof, n)
+        function [G0, G1] = tractionOp(obj, udof, n)
             % tractionOp traction operator 
             cn = obj.mat.c/obj.mat.c(1,2,1,2); % normalized stiffness tensor
             % relevant material matrices: 
@@ -46,13 +64,32 @@ classdef LayerCylindrical < Layer
             crr = squeeze(cn(2,udof,udof,2));
             crp = squeeze(cn(2,udof,udof,3));
             A = [0, 0, 0; 0, 0, -1; 0, 1, 0]; A = squeeze(A(udof, udof)); % differetiation in curvilinear coordinate system
-            % differentiation matrices on normalized domain 
-            Dr1 = obj.D1; rInv = diag(1./obj.r); 
-            Id = eye(obj.N); % identity matrix for discretization
-            % operators:
-            B1 = kron(crx, Id([1, obj.N], :)); 
-            B0 = kron(crr, Dr1([1, obj.N], :)) + kron(crp*A, rInv([1, obj.N], :)) + 1i*n*kron(crp, rInv([1, obj.N], :)); 
+            I = eye(size(A));
+            % normalized element flux:
+            G1 = kron( crx , -obj.PPd.' );
+            G0 = kron( crr , -obj.PdPd.' )  +  kron( crp*(1i*n*I + A) , -obj.PPdr.' );
         end
         
+    end % methods
+
+    methods (Static)
+        function ppr = elemPPr(P, w, r) 
+            % elemPPr: integral ∫P*P*1/r dr of basis functions P
+            PtimesP = P.*permute(P,[1 3 2]); % size: [nodal points, len P, len P]
+            PtimesPr = (1./r(:)).*PtimesP;
+            ppr = squeeze( sum(w.'.*PtimesPr,1) );
+        end
+        function ppr2 = elemPPr2(P, w, r) 
+            % elemPPr2: integral ∫P*P*1/r^2 dr of basis functions P
+            PtimesP = P.*permute(P,[1 3 2]); % size: [nodal points, len P, len P]
+            PtimesPr = (1./r(:).^2).*PtimesP;
+            ppr2 = squeeze( sum(w.'.*PtimesPr,1) );
+        end
+        function ppdr = elemPPdr(P, Pd, w, r) 
+            % elemPPdr: integral ∫P*P'*1/r dr of basis functions P
+            PtimesPd = P.*permute(Pd,[1 3 2]); % size: [nodal points, len P, len P]
+            PtimesPdr = (1./r(:)).*PtimesPd;
+            ppdr = squeeze( sum(w.'.*PtimesPdr,1) );
+        end
     end
 end
