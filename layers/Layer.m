195aa40e1bb5eeccfd0b8fd8b483d4653ef4030f
Daniel Kiefer <daniel.kiefer@fau.de>
2022-05-23 15:38:07 +0200

    Layer: moved creation of nodes and basis functions to static method

--- layers/Layer.m
+++ layers/Layer.m
@@ -20,22 +20,9 @@ classdef Layer
             obj.y = chebpts(N, ylim); % computing grid: defines polynomial order
             obj.h = ylim(end) - ylim(1); % physical thickness
             
-            % % construct element matrices (Chebyshev polynomials on Chebyshev points): 
-%             [yi, wi] = chebpts(2*N, [0 1]); % integration weights: integrate exactly P*P
-%             Dy = diffmat(2*N, [0 1]); % differentiation matrix on integration grid yi
-%             Psi = chebpoly(0:N-1, [0 1]); % Chebyshev polynomials
-%             P = Psi(yi,:); % along 1st dim: samples at yi, along 2nd dim: polynomial order
-%             Pd = squeeze(sum(Dy.*shiftdim(P, -1), 2)); % differentiated polynomials
-
-            % % For Lagrange polynomials with GLL points:
-            [yi, wi] = lobpts(N, [-1, 1]); % does only work on dom = [-1 1]!!!!
-            wi = wi/2; yi = yi/2 + 1/2; % scale to [0, 1]
-            Psi = chebfun.lagrange(yi);
-            Psid = diff(Psi);
-            P = eye(length(yi)); % Psi(yi,:);
-            Pd = Psid(yi,:);
-            
             % % element matrices:
+            [yi, wi] = Layer.nodes(N);  % nodal coordinates and integration weights
+            [P, Pd] = Layer.basis(yi);  % polynomial basis
             obj.PP = Layer.elemPP(P, wi);
             obj.PPd = Layer.elemPPd(P, Pd, wi);
             obj.PdPd = Layer.elemPdPd(Pd, wi);
@@ -72,6 +59,30 @@ classdef Layer
             PdtimesPd = Pd.*permute(Pd,[1 3 2]);
             g0 = squeeze( sum(-w.'.*PdtimesPd,1) );
         end
+
+        function [yi, wi] = nodes(N)
+            % % For Chebyshev polynomials on Chebyshev points: 
+            % [yi, wi] = chebpts(2*N, [0 1]); % integration weights: integrate exactly P*P
+
+            % % For Lagrange polynomials with GLL points:
+            [yi, wi] = lobpts(N, [-1, 1]); % does only work on dom = [-1 1]!!!!
+            wi = wi/2; yi = yi/2 + 1/2; % scale to [0, 1]
+        end
+
+        function [P, Pd] = basis(yi)
+            % % For Chebyshev polynomials: 
+%             Dy = diffmat(2*N, [0 1]); % differentiation matrix on integration grid yi
+%             Psi = chebpoly(0:N-1, [0 1]); % Chebyshev polynomials
+%             P = Psi(yi,:); % along 1st dim: samples at yi, along 2nd dim: polynomial order
+%             Pd = squeeze(sum(Dy.*shiftdim(P, -1), 2)); % differentiated polynomials
+
+            % % For Lagrange polynomials
+            Psi = chebfun.lagrange(yi);
+            Psid = diff(Psi);
+            P = eye(length(yi)); % Psi(yi,:);
+            Pd = Psid(yi,:);
+        end
+
     end
 
     methods (Abstract)
