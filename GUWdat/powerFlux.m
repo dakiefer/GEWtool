37dea6e33500801a5a83ffd367028332df26ace0
Daniel Kiefer <daniel.kiefer@fau.de>
2022-06-01 17:14:51 +0200

    GUWdat: updated to use GEWintegrate()

--- GUWdat/powerFlux.m
+++ GUWdat/powerFlux.m
@@ -2,12 +2,10 @@ function [P] = powerFlux(wguide, dat)
 %POWERFLUX Compute total power flux along the waveguide.
 
 p = poyntingVec(wguide, dat);
-
-P = zeros(size(dat.k)); 
 for i = 1:wguide.geom.nLay
-    px = p{i}(:,:,:,1);             % power flux density along waveguide;
-    lims = wguide.geom.yItf(i,:);   % lower and upper integration limits
-    P = P + chebintegrate(px, lims, 3); % along 3rd dimension
+    px{i} = p{i}(:,:,:,1);        % power flux density along waveguide;
 end
+
+P = GEWintegrate(wguide, px);
 
 end
