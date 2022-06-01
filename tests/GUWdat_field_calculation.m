e4a7e640fe8b6dbb185541c46713fe643f40d47b
Daniel Kiefer <daniel.kiefer@fau.de>
2022-06-01 17:20:30 +0200

    test: updated GUWdat_field_calculation test

--- tests/GUWdat_field_calculation.m
+++ tests/GUWdat_field_calculation.m
@@ -1,7 +1,7 @@
-%% EDAT:
+% % EDAT:
 mat = Material.Material('brass'); 
 h = 1e-3; % plate thickness in mm
-N = 25; % number of collocation points
+N = 40; % number of collocation points
 plate = Dispersion.Plate(mat, h, N);
 
 % compute wavenumbers and wave displacement structures:
@@ -9,20 +9,24 @@ freq = linspace(0.01, 4, 150).'*1e6;
 Nmodes = 60;
 data = plate.solveForFreq(freq, Nmodes);
 ff = freq.*ones(size(data.k)); % for plotting
+[ySCM, wSCM] = chebpts(N, [-0.5 0.5]*h);
+data.u = normalizeL2(data.u, wSCM);  % normalize to ∫ conj(u).u dy = 1
 
-%% new implementation:
+% % new implementation:
 clear plate
 plate = Plate(mat, h, N);
 wguide = plate.Lamb;
 % k = linspace(1e-3, 15, 150)/h;
 dat = computeK(wguide, 2*pi*freq);
+[yNew, wNew] = Layer.nodes(N); wNew = wNew*h; yNew = (yNew-0.5)*h;
+dat.u{1} = normalizeL2(dat.u{1}, wNew); % % normalize to ∫ conj(u).u dy = 1
 
 %% displacements:
 uOld = abs(data.u(:,:,1,1));
 uNew = abs(dat.u{1}(:,:,1,1));
-figure, hold on, title('displacement ux at boundary')
-plot(ff(:), real(uOld(:)), 'x')
-plot(dat.w(:)/2/pi, real(uNew(:)), '.')
+clf, hold on, title('displacement ux at boundary')
+plot(ff(:), abs(uOld(:)), 'x')
+plot(dat.w(:)/2/pi, abs(uNew(:)), '.')
 
 %% particle velocity:
 vOld = abs(data.v(:,:,1,1));
@@ -41,7 +45,7 @@ Snew = Snew(:,:,1,1,1);
 figure, hold on, title('strain Sxx at boundary')
 plot(ff(:), real(Sold(:)), 'x')
 plot(dat.w(:)/2/pi, Snew(:), '.')
-ylim([0, 5000])
+% ylim([0, 5000])
 
 %% stress: 
 TOld = abs(data.T(:,:,1,1,1));
@@ -50,7 +54,16 @@ Tnew = abs(Tnew{1});
 Tnew = Tnew(:,:,1,1,1);
 figure, hold on, title('stress Txx at boundary')
 plot(ff(:), real(TOld(:)), 'x')
-plot(dat.w(:)/2/pi, real(Tnew(:)), '.'), ylim([0, 3e14])
+plot(dat.w(:)/2/pi, real(Tnew(:)), '.'), %ylim([0, 3e14])
+
+%% power flux at interface 
+pOld = data.poyntingVectors;
+pOld = pOld(:,:,1,1);
+pNew = poyntingVec(wguide, dat);
+pNew = pNew{1}(:,:,1,1);
+figure, hold on, title('power flux density px at boundary')
+plot(ff(:), real(pOld(:)), 'x')
+plot(dat.w(:)/2/pi, real(pNew(:)), '.')
 
 %% kinetik energy 
 EkOld = data.Ek;
@@ -65,15 +78,6 @@ EsNew = energyElastic(wguide, dat);
 figure, hold on, title('total elastic energy')
 plot(ff(:), abs(EsOld(:)), 'x');
 plot(dat.w(:)/2/pi, abs(EsNew(:)), '.');
-
-%% power flux at interface 
-pOld = data.poyntingVectors;
-pOld = pOld(:,:,1,1);
-pNew = poyntingVec(wguide, dat);
-pNew = pNew{1}(:,:,1,1);
-figure, hold on, title('power flux density px at boundary')
-plot(ff(:), real(pOld(:)), 'x')
-plot(dat.w(:)/2/pi, real(pNew(:)), '.')
 
 %% power flux total
 Pold = data.Px;
