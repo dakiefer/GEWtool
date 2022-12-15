2cab8840ceb89ac9980ce14f3a1e34a12bb8b86f
Daniel Kiefer <daniel.kiefer@fau.de>
2022-12-15 21:43:04 +0100

    tests/test_disperse_vs_GEWtool: request user confirmation

--- tests/test_disperse_vs_GEWtool.m
+++ tests/test_disperse_vs_GEWtool.m
@@ -5,31 +5,56 @@ mat = Material('noname', lbd, mu, rho);
 N = 12;
 k = linspace(1e-3, 0.5, 100)/h; % wavenumber-thickness (solve for frequency)
 cyl = Cylinder(mat, [a, b], N);
+load('data/disperse.mat')
 
-%% zeroth-order circumferential waves:
-n = 0;
-gew = cyl.fullyCoupled(n);
-dat = computeW(gew, k); 
+%% plot
+gew = cyl.fullyCoupled(0); % first order circumferential
+dat0 = computeW(gew, k); 
+gew = cyl.fullyCoupled(1); % second order circumferential
+dat1 = computeW(gew, k); 
 
 % plot against reference data:
-load('data/disperse.mat')
-figure, hold on
-plot(disperse.k/1e-3, disperse.f*1e6, 'g.')
+fig = figure; hold on
 xlim([0, 0.5]), ylim([0, 200])
 xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
+plot(disperse.k/1e-3, disperse.f*1e6, 'g.')
 ax = gca; ax.ColorOrderIndex = 1; % reset color order 
-hold on, plot(dat.k(:), dat.w(:)/2/pi, '.'); 
-legend({'disperse', 'n = 0'})
+plot(dat0.k(:), dat0.w(:)/2/pi, '.'); 
+plot(dat1.k(:), dat1.w(:)/2/pi, '.'); 
+legend({'disperse', 'n = 0', 'n = 1'})
 
-n = 1;
-gew = cyl.fullyCoupled(n);
-dat = computeW(gew, k); 
-hold on, plot(dat.k(:), dat.w(:)/2/pi, '.'); 
-legend({'disperse', 'n = 0'})
+% request user to evaluate test:
+answer = questdlg('Is the plot good?','confirm test', 'yes', 'no', 'no');
+close(fig);
+pass = false;
+if strcmp(answer, 'yes'), pass = true; end
+assert(pass)
 
-%% fist-order circumferential waves:
-% n = 1;
-% gew = cyl.fullyCoupled(n);
-% dat = computeW(gew, k); 
-% hold on, plot(dat.k(:), dat.w(:)/2/pi, '.'); 
-% legend({'disperse', 'n = 0', 'n = 1'})
+% some unused stuff:
+% pass = uiconfirm(fig, 'pass?','Is plot good?')
+% uiwait(fig)
+% function [fig, ax] = figureConfirm()
+% fig = uifigure('WindowButtonDownFcn',@(src,event)uiresume(src));
+% ax = uiaxes('Parent',fig,...
+%             'Units','pixels',...
+%             'Position', [25, 70, 500, 340]);   
+% passBtn = uibutton(fig,'push',...
+%                'Text', 'pass',...
+%                'Position',[200, 10, 100, 40],...
+%                'UserData',false,...
+%                'ButtonPushedFcn', @(btn, event) passButtonPushed(btn, fig));
+% failBtn = uibutton(fig,'push',...
+%                'Text', 'fail',...
+%                'Position',[350, 10, 100, 40],...
+%                'UserData',false,...
+%                'ButtonPushedFcn', @(btn, event) passButtonPushed(btn, fig));
+% end
+% function passButtonPushed(btn, fig)  
+%         btn.UserData = true; 
+%         disp(btn.Text);
+%         close(fig);
+% end
+% function choice = waitForButtonPress(fig)
+%     uiwait(fig); % wait for button press
+%     btns = findobj(fig, 'Type', 'uibutton');
+% end
