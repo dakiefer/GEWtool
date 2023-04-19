ab01cd67c93340501fdd856b4a8953afa8f11dda
Daniel Kiefer <daniel.kiefer@fau.de>
2023-04-19 19:01:48 +0200

    numerics/ZGV_eigs(): renamed variables to mach manuscript.

--- utilitiesAndNumerics/ZGV_eigs.m
+++ utilitiesAndNumerics/ZGV_eigs.m
@@ -1,4 +1,4 @@
-function [X,D] = ZGV_eigs(L2,L1,L0,M,epsilon,Neigs,shift)
+function [X,D] = ZGV_eigs(L2,L1,L0,M,delta,Neigs,shift)
 % ZGV_eigs - compute eigenvalues close to a target of a MEP.
 % 
 % 2023 - written by Bor Plestenjak for ZGV_Sylv_MFRDScan().
@@ -6,18 +6,18 @@ function [X,D] = ZGV_eigs(L2,L1,L0,M,epsilon,Neigs,shift)
 m = size(L0,1);
 n = m^2;
 
-S = L0 + shift*L1 + shift^2*L2;
-Seps = L0 + (1+epsilon)*shift*L1 + (1+epsilon)^2*shift^2*L2;
-S1 = transpose(S)/M;
-S1eps = M\Seps;
+L = L0 + shift*L1 + shift^2*L2;
+Ldel = L0 + (1+delta)*shift*L1 + (1+delta)^2*shift^2*L2;
+LM = transpose(L)/M;    % = B in A*X + X*B = C
+MLdel = M\Ldel;         % = A in A*X + X*B = C
 
-[Q1,R1] = schur(-S1eps,'complex');
-[Q2,R2] = schur(S1,'complex');
+[Q,R] = schur(-MLdel,'complex');
+[U,S] = schur(LM,'complex');
 
 F1 = transpose(L1+shift*L2)/M;
 F2 = transpose(L2)/M;
-F3 = M\((1+epsilon)*L1+shift*(1+epsilon)^2*L2);
-F4 = (1+epsilon)^2*(M\L2);
+F3 = M\((1+delta)*L1+shift*(1+delta)^2*L2);
+F4 = (1+delta)^2*(M\L2);
 
 [X,D] = eigs(@multGamma,2*m^2,Neigs);
 
@@ -28,8 +28,8 @@ function x = multGamma(y)
     Y1 = reshape(y(1:n),m,m);
     Y2 = reshape(y(n+1:2*n),m,m);
     C = Y1*F1 - F3*Y1 + Y2*F2 - F4*Y2;
-    Z1 = sylvester(R1,R2,-Q1'*C*Q2);
-    X1 = Q1*Z1*Q2';
+    Z1 = sylvester(R,S,-Q'*C*U);
+    X1 = Q*Z1*U';
     X2 = Y1 + shift*X1;
     x = [reshape(X1,m^2,1); reshape(X2,m^2,1)];
 end
