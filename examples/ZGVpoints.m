07b8a68b3c2c38114d6e65e6f64a4f5eaf3690a9
Daniel Kiefer <daniel.kiefer@fau.de>
2023-04-21 18:03:20 +0200

    examples/ZGVpoints: adapted to new methods. New plotting.

--- examples/ZGVpoints.m
+++ examples/ZGVpoints.m
@@ -15,7 +15,7 @@ mat = Material('steel_austenitic'); % orthotropic
 mat = mat.rotateEuler(90/180*pi, 90/180*pi, 0); % this direction is fun
 h = 1e-3; % thickness 
 N = 25; % number of discretization points
-fmax = 15e6; % maximum frequency for plotting and ZGV-search
+wmax = 2*pi*15e6; % maximum frequency for plotting and ZGV-search
 k = linspace(1e-2, 40, 150)/h; % wavenumbers for plotting dispersion curves
 plate = Plate(mat, h, N); % create waveguide description 
 gew = plate.LambA; % assembles matrices for the specified waves
@@ -26,22 +26,35 @@ dat.cg = groupVelAxial(gew, dat); % compute group velocity for plotting
 w0 = [0.29    0.52    0.69    0.72    0.75]*1e8;
 k0 = [0.34    0.35    0.71    0.25    0.07]*1e4;
 
+% % plot dispersion curves for reference: 
+fig = figure; clf, hold on, grid off;
+plot(dat.k*h, dat.w*h/2/pi/1e3, 'Color', [.5, .5, .5], 'HandleVisibility', 'off');
+ylim([0, wmax/2/pi/1e6]), xlim([0, wmax/2/pi/1e6]), legend('Location','southeast')
+set(fig,'defaulttextinterpreter','latex'), set(fig, 'DefaultLegendInterpreter', 'latex')
+xlabel('wavenumber-thickness $kh$ in rad'), ylabel('frequency-thickness $fh$ in MHz$\cdot$mm')
+drawnow;
+
 %% Newton-kind iteration: 
 % This method is implemented in computeZGV(). It is super fast but needs initial
 % guesses:
 fprintf('\n\n++ Newton-type iteration: ++\n')
 fprintf('Search close to provided initial guesses:\n')
 tic, zgv5 = computeZGV(gew, w0, k0); toc
-w0initial = w0, wConverged = zgv5.w.' % print initial guesses and computed values
+% print initial guesses and computed values 
+disp('initial frequencies:'), disp(w0/2/pi)
+disp('converged frequencies:'), disp(zgv5.w.'/2/pi)
 
 % Instead of initial guesses w0, k0, you can also provide the dispersion data
 % "dat", computeZGV() will then search where the group velocity changes sign:
 fprintf('\n\n++ Newton-type iteration: ++\n')
 fprintf('Search where group velocity changes sign:\n')
-tic, zgv = computeZGV(gew, dat); toc
-nZGV = length(zgv.w(zgv.w < 2*pi*fmax)) % print number of ZGV points 
-plotZGVs(dat, zgv, fmax), title('Newton-type method'), drawnow
+tic, zgvNewton = computeZGV(gew, dat); timing = toc;
+nZGV = length(zgvNewton.w(zgvNewton.w < wmax)); 
+fprintf('Computed %d ZGV points in %g s.\n', nZGV, timing); 
 
+figure(fig);
+plot(zgvNewton.k(:)*h, zgvNewton.w(:)*h/2/pi/1e3, 'rx', 'MarkerSize', 10, 'DisplayName', 'Newton method');
+drawnow;
 
 %% Scanning the ZGV points
 % This methods is implemented in computeZGVScan(). It does not need initial
@@ -51,10 +64,15 @@ plotZGVs(dat, zgv, fmax), title('Newton-type method'), drawnow
 % kMax (and optionally kStart), these define the wavenumber search interval
 % [kStart, kMax].
 fprintf('\n\n++ Scanning method: ++\n')
-clear opts, opts.kMax = 10/gew.np.h0; % unit distance is np.h0
-tic, zgv = computeZGVScan(gew, opts); toc
-nZGV = length(zgv.w(zgv.w < 2*pi*fmax)) % print number of ZGV points 
-plotZGVs(dat, zgv, fmax), title('Scaning method'), drawnow
+clear opts, opts.kEnd = 12/h; opts.show = false; % unit distance is np.h0
+tic, zgvScan = computeZGVScan(gew, wmax, opts); timing = toc;
+nZGV = length(zgvScan.w(zgvScan.w < wmax));
+fprintf('Computed %d ZGV points in %g s.\n', nZGV, timing); 
+
+figure(fig);
+plot(zgvScan.k(:)*h, zgvScan.w(:)*h/2/pi/1e3, 'ksquare', 'DisplayName', 'Scanning method', 'MarkerSize', 8);
+if isfield(zgvScan, 'k0s'), xline(zgvScan.k0s*h, 'HandleVisibility', 'off'); end
+drawnow;
 
 %% Direct method
 % This method is implemented in computeZGVDirect(). It does not need initial
@@ -62,23 +80,10 @@ plotZGVs(dat, zgv, fmax), title('Scaning method'), drawnow
 % the problem are not too big. This is rather slow and should not be used for
 % matrices bigger than about 40x40. 
 fprintf('\n\n++ Direct method: ++\n')
-tic, zgv = computeZGVDirect(gew); toc
-nZGV = length(zgv.w(zgv.w < 2*pi*fmax)) % print number of ZGV points 
-plotZGVs(dat, zgv, fmax), title('Direct method'), drawnow
+tic, zgvDirect = computeZGVDirect(gew); timing = toc;
+nZGV = length(zgvDirect.w(zgvDirect.w < wmax));
+fprintf('Computed %d ZGV points in %g s.\n', nZGV, timing); 
 
-
-%% function to plot dispersion curves with ZGV points
-function plotZGVs(dat, zgv, fmax)
-    fh = figure; pos = fh.Position; fh.Position = [pos(1), pos(2)-pos(4)*0.8, pos(3), pos(4)*1.8];
-
-    subplot(2,1,1), hold on, ylim([0, fmax]); xlim([0, 25e3])
-    plot(dat.k, dat.w/2/pi, '-'); ax = gca; ax.ColorOrderIndex = 1; 
-    xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
-    plot(zgv.k(:), zgv.w(:)/2/pi, 'r*');
-    
-    subplot(2,1,2), hold on, xlim([0, fmax]); 
-    plot(real(dat.w)/2/pi, real(dat.cg), '-'); ax = gca; ax.ColorOrderIndex = 1; 
-    xlabel('frequency f in Hz'), ylabel('group vel in m/s')
-    plot(zgv.w(:)/2/pi, zeros(size(zgv.w(:))), 'r*')
-    subplot(2,1,1);
-end
+figure(fig);
+plot(zgvDirect.k(:)*h, zgvDirect.w(:)*h/2/pi/1e3, 'k.', 'MarkerSize', 8, 'DisplayName', 'Direct method');
+drawnow;
