464c21454a3adb19d8b3c17540ddb7d435d35fc8
Daniel Kiefer <daniel.kiefer@fau.de>
2023-04-26 17:44:34 +0200

    example/Lamb_waves_plate: updated example

--- examples/Lamb_waves_plate.m
+++ examples/Lamb_waves_plate.m
@@ -1,27 +1,31 @@
-%% compute guided waves in a plate
+%% Compute Lamb waves in a plate
+% This example shows how to compute Lamb waves. The first approach shown
+% computes all Lamb waves. The second block computes the symmetric and
+% anti-symmetric waves separately. This is faster and the modes are correctly
+% orderd. The third section computes the complex wavenumber spectrum. 
 % 
-% 2022 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+% 2022-2023 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
 mat = Material('steel');        % load from database (or create your own)
 h = 1e-3;                       % thickness in m
-N = 12;                         % number of nodes (dictates accuracy)
+N = 14;                         % number of nodes (dictates accuracy)
 k = linspace(1e-2, 12, 100)/h;  % wavenumbers to solve for
 plate = Plate(mat, h, N);       % create waveguide description 
 
 %% compute Lamb waves (any symmetry):
 gew = plate.Lamb; tic           % choose waves (assemble matrices) 
 dat = computeW(gew, k); toc     % solve
+figure(1); clf; 
 plot(dat.k/1e3, dat.w/2/pi/1e6); ylim([0, 6]);   % plot
 xlabel('wavenumber k in rad/mm'), ylabel('frequency f in MHz')
 title('Lamb waves')
 
 %% compute symmetric/anti-symmetric waves separately:
-gews = plate.LambSA; % assembles matrices for both the sym/anti-sym Lamb waves
-datSA = computeW(gews, k);
-
+gews = plate.LambSA; tic; % assembles matrices for both the sym/anti-sym Lamb waves
+datSA = computeW(gews, k); toc;
 datS = datSA(1); % symmetric waves
 datA = datSA(2); % anti-symmetric waves
-figure, hold on, cc = lines(3);
+figure(2); clf; hold on, cc = lines(3);
 hS = plot(datS.k/1e3, datS.w/2/pi/1e6, 'Color', cc(1,:)); 
 hA = plot(datA.k/1e3, datA.w/2/pi/1e6, 'Color', cc(2,:)); ylim([0, 6]);
 xlabel('wavenumber k in rad/mm'), ylabel('frequency f in MHz')
@@ -29,9 +33,12 @@ legend([hS(1), hA(1)], {'symmetric', 'anti-symmetric'}, 'Location', 'southeast')
 title('symmetric and anti-symmetric Lamb waves')
 
 %% compute wavenumbers and plot:
-freq = linspace(1e-2, 6, 500).'*1e6; % frequencies where to compute wavenumbers k
-dat = computeK(gew, 2*pi*freq);
-figure, plot3(real(dat.k(:))/1e3, imag(dat.k(:))/1e3, dat.w(:)/2/pi/1e6, '.'); 
+w = 2*pi*linspace(1e-2, 6, 600).'*1e6; tic; % frequencies where to compute wavenumbers k
+% linearizeInK(gews); % optional: this makes the computation faster
+datCompl = computeK(gews, w); toc;
+figure(3); clf; hold on, cc = lines(3);
+plot3(real(datCompl(1).k(:))/1e3, imag(datCompl(1).k(:))/1e3, datCompl(1).w(:)/2/pi/1e6, '.', 'Color', cc(1,:)); 
+plot3(real(datCompl(2).k(:))/1e3, imag(datCompl(2).k(:))/1e3, datCompl(2).w(:)/2/pi/1e6, '.', 'Color', cc(2,:)); 
 xlim([0, 12]), ylim([-10.5, 10.5]), view(-22, 18)
 xlabel('Re(k) in rad/mm'), ylabel('Im(k) in rad/mm')
 zlabel('f in MHz')
