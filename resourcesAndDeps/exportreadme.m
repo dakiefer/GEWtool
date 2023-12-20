9a17d200196b0c0aa74486e72cb6bcfffddd620b
Daniel Kiefer <daniel.kiefer@fau.de>
2023-12-20 12:16:13 +0100

    exportreadme.m: using pandoc for pdf creation.

--- resourcesAndDeps/exportreadme.m
+++ resourcesAndDeps/exportreadme.m
@@ -3,16 +3,19 @@ function exportreadme(fformat)
     if nargin < 1
         fformat = "";
     end
-    setenv('PATH', getenv('PATH')+":/opt/homebrew/bin:/usr/local/bin")
+    setenv('PATH', getenv('PATH')+":/opt/homebrew/bin:/usr/local/bin:/Library/TeX/texbin")
     cmdHTML = 'grip --export readme.md';
-    cmdPDF = "pandoc -s -f gfm -t html5 --metadata-file resourcesAndDeps/readme_format.yaml --metadata title="""" --pdf-engine wkhtmltopdf readme.md -o readme.pdf";
+    % cmdPDF = "pandoc -s -f gfm -t html5 --metadata-file resourcesAndDeps/readme_format.yaml --metadata title="""" --pdf-engine wkhtmltopdf readme.md -o readme.pdf"; old export
+    cmdPDF = "pandoc -s --pdf-engine xelatex -V geometry:""left=2.5cm,right=2.5cm,top=1.5cm,bottom=2cm"" readme.md -o readme.pdf";
     switch fformat
         case "html"
             system(cmdHTML);
         case "pdf"
+            disp('Exporting to readme.pdf')
             system(cmdPDF); 
         otherwise
             system(cmdHTML);
+            disp('Exporting to readme.pdf')
             system(cmdPDF); 
     end
 end
