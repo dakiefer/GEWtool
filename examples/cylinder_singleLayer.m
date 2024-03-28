c5b2a24005ffbbbd7bd9dc0f88659a85100fee21
Daniel Kiefer <daniel.kiefer@fau.de>
2024-03-28 16:52:41 +0100

    example added: full rod. updated cylinder_singleLayer.

--- examples/cylinder_singleLayer.m
+++ examples/cylinder_singleLayer.m
@@ -1,24 +1,24 @@
-%% compute axially guided ultrasonic waves in a cylinder
-% different circumferential orders n of the waves are compared
+%% Compute axially guided waves in a tube
+% Different circumferential orders n of the waves are compared.
 % 
 % 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
 % % specify parameters:
-mat = MaterialIsotropic('steel'); % load from material/database
-r = [10e-3, 11e-3]; h = r(end) - r(1); % radii and thickness 
-N = 8; % number of discretization points
-k = linspace(0, 4, 300)/h; % wavenumbers to solve for
+mat = MaterialIsotropic('steel');       % load from material/database
+r = [10e-3, 11e-3]; h = r(end) - r(1);  % radii and thickness 
+N = 8;                                  % number of nodes
+k = linspace(0, 4, 200)/h; % wavenumbers to solve for
 cyl = Cylinder(mat, r, N); % create waveguide description 
 
 %% compute and plot
 n = 0:1:2; % circumferential order to solve for
-figure, hold on; ax = gca; xlim([0, k(end)]); clear ph;
-xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
+figure, hold on; ax = gca; xlim([0, k(end)/1e3]); clear ph;
+xlabel('wavenumber k in rad/mm'), ylabel('frequency f in MHz')
 cc = winter(length(n));
 for i = 1:length(n)
     gew = cyl.fullyCoupled(n(i)); % assembles matrices for the specified waves
     dat = computeW(gew, k); 
-    phs = plot(dat.k, dat.w/2/pi, 'Color', cc(i,:)); ph(i) = phs(1);
-    ylim([0, 2e3]/h); drawnow;
+    phs = plot(dat.k/1e3, dat.w/2/pi/1e6, 'Color', cc(i,:)); ph(i) = phs(1);
+    ylim([0, 2]); drawnow;
 end
 legend(ph, strcat('n = ', num2str(n.')), 'Location', 'southeast')
---
+++ examples/rod.m
@@ -0,0 +1,40 @@
+%% Compute waves in a circular rod
+% Solve for waves travelling axially along a non-hollow circular rod. GEWtool
+% automatically switches the Finite Element integration scheme to avoid the
+% singularity at r=0 that appears in the 1/r-term. Only waves of 0th order
+% circumferential order are computed. These segregate into two families of
+% waves: torsional (uPhi-polarized) and longitudinal (ux-ur-polarized). 
+% 
+% 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+mat = MaterialIsotropic('steel');  % load from material/database
+r = 1e-3;                          % radius r
+N = 10;                            % number of nodes (dictates accuracy)
+k = linspace(0, 15, 200)/r;        % wavenumbers to solve for
+cyl = Cylinder(mat, [1e-3, 1e-3+r], N); tic;   % create waveguide description 
+
+% compute torsional waves (uPhi displacements only)
+tors = cyl.torsional;              % assembles matrices
+datT = computeW(tors, k);
+
+% compute longitudinal waves (ux and ur displacements only)
+long = cyl.longitudinal;           % assembles matrices 
+datL = computeW(long, k); toc
+
+% plot frequency-wavenumber dispersion 
+figure(1); clf; hold on
+hT = plot(datT.k/1e3, datT.w/2/pi/1e6, 'Color', "#D95319"); % plot torsional in red
+hL = plot(datL.k/1e3, datL.w/2/pi/1e6, 'Color', "#0072BD"); % plot longitudinal in blue
+ylim([0, 6]); % frequency range
+xlabel('wavenumber k in rad/mm'), ylabel('frequency f in MHz')
+legend([hT(1), hL(2)], {'torsional', 'longitudinal'}, 'Location','south east')
+title(sprintf('rod of radius %g mm', r/1e-3))
+
+% plot phase velocity-frequency dispersion 
+figure(2); clf; hold on; 
+hT = plot(datT.w/2/pi/1e6, datT.w./datT.k/1e3, 'Color', "#D95319"); % plot torsional in red
+hL = plot(datL.w/2/pi/1e6, datL.w./datL.k/1e3, 'Color', "#0072BD"); % plot longitudinal in blue
+xlim([0, 6]); ylim([0, mat.cl*2/1e3]) 
+ylabel('phase velocity in mm/us'), xlabel('frequency f in MHz')
+legend([hT(1), hL(2)], {'torsional', 'longitudinal'}, 'Location','south east')
+title(sprintf('rod of radius %g mm', r/1e-3))
