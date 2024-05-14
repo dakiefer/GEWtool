6531a67c04ae2f448037cf094f377b01f9b1c9a5
Daniel Kiefer <daniel.kiefer@fau.de>
2024-05-14 18:38:55 +0200

    reference implementations: updated SCM example script for axially guided waves in a cylinder

--- reference implementations/cylindrical/axial_gews.m
+++ reference implementations/cylindrical/axial_gews.m
@@ -1,127 +1,101 @@
-%% compute axially guided ultrasonic waves in cylindrical coordinates
+%% Axially guided waves in a cylindrical tube 
+% The spectral collocation method (SCM) is implemented to solve the boundary-value
+% problem describing waves guided along the axis of a cylindrical tube.
+%
+% See also 
+% [1] A. T. I. Adamou and R. V. Craster, “Spectral methods for modelling guided
+% waves in elastic media,” The Journal of the Acoustical Society of America,
+% vol. 116, no. 3, pp. 1524–1535, Sep. 2004, doi: 10.1121/1.1777871.
+% [2] D. A. Kiefer, Elastodynamic quasi-guided waves for transit-time ultrasonic
+% flow metering. Erlangen: FAU University Press, 2022. 
+% doi: 10.25593/978-3-96147-550-6.
+% [3] J. Zemanek Jr., “An Experimental and Theoretical Investigation of Elastic
+% Wave Propagation in a Cylinder,” The Journal of the Acoustical Society of
+% America, vol. 51, no. 1B, pp. 265–283, Jan. 1972, doi: 10.1121/1.1912838.
 % 
-% see also:
-% [1] M. Zheng, C. He, Y. Lyu, and B. Wu, “Guided waves propagation in anisotropic 
-% hollow cylinders by Legendre polynomial solution based on state-vector formalism,” 
-% Composite Structures, vol. 207, pp. 645–657, Jan. 2019, 
-% doi: 10.1016/j.compstruct.2018.09.042.
-% 
-% 2022 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+% 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+%             Pierre Chantelot, Institut Langevin, ESPCI Paris, France
 
 % specify parameters:
-a = 3e-3; b = 5e-3; % inner and outer radius
-n = 0; % circumferential wavenumber (flexural order)
-N = 20; % number of collocation points
-kh = linspace(1e-2, 15, 400); % wavenumber-thickness (solve for frequency)
-% fh = linspace(1e-2, 6, 300).'*1e3; % frequency-thickness (solve for wavenumbers)
+a = 5e-3;       % inner radius
+h = 1e-3;       % thickness of the shell
+n = 0;          % flexural order (circumferential wavenumber)
+N = 15;         % number of collocation points
+k = linspace(1e-2, 15, 70).'/h; % wavenumber-thickness (solve for frequency)
 rho = 7900; lbd = 1.1538e11; mu = 7.6923e10; % material parameters
+
+% verify that chebfun is installed:
+if ~exist('chebdif','file')
+    error('You need to have DMSUITE installed to use this example. See https://fr.mathworks.com/matlabcentral/fileexchange/29-dmsuite');
+end
+if a == 0
+    error('You cannot use this code to compute solid cylinders but you can set the inner radius very small.');
+end
+
+% % derived parameters and normalization:
+b = a + h; % outer radius
 II = eye(3).*shiftdim(eye(3), -2); % 4th order "unit tensor"
 c = lbd*II + mu*(permute(II, [1 3 4 2]) + permute(II, [1 3 2 4])); % stiffness tensor
-
-%% derived parameters and normalize parameters:
-h = b-a; % thickness
-c0 = c(1,2,1,2); h0 = h; % normalization parameters
-rho0 = rho; f0 = sqrt(c0/rho0)/h0; % normalization parameters
-rhon = rho/rho0; cn = c/c0;
-udof = [1, 2, 3];
-
+c0 = c(1,2,1,2); rho0 = rho; fh0 = sqrt(c0/rho0); % normalization parameters
+rhon = rho/rho0; cn = c/c0; % normalized
 % relevant material matrices: 
-cxx = squeeze(cn(1,udof,udof,1));
-crr = squeeze(cn(2,udof,udof,2));
-cpp = squeeze(cn(3,udof,udof,3));
-Crp = squeeze(cn(2,udof,udof,3)) + squeeze(cn(3,udof,udof,2));
-Cxp = squeeze(cn(1,udof,udof,3)) + squeeze(cn(3,udof,udof,1));
-Crx = squeeze(cn(2,udof,udof,1)) + squeeze(cn(1,udof,udof,2));
-crp = squeeze(cn(2,udof,udof,3));
-crx = squeeze(cn(2,udof,udof,1));
+cxx = squeeze(cn(1,:,:,1));
+crr = squeeze(cn(2,:,:,2));
+cpp = squeeze(cn(3,:,:,3));
+crp = squeeze(cn(2,:,:,3));
+cpr = squeeze(cn(3,:,:,2));
+cxp = squeeze(cn(1,:,:,3));
+cpx = squeeze(cn(3,:,:,1));
+crx = squeeze(cn(2,:,:,1));
+cxr = squeeze(cn(1,:,:,2));
 A = [0, 0, 0; 0, 0, -1; 0, 1, 0]; % differetiation in curvilinear coordinate system
-B = [0, 0, 0; 0, 1,  0; 0, 0, 1]; % differetiation in curvilinear coordinate system
-A = A(udof, udof); B = B(udof, udof);
-rho = rhon*eye(size(crr)); % expand to matrix
-
-% cpp = 0*cpp; Czp = 0*Czp; Crp = 0*Crp; crp = 0*crp;
+I = eye(size(A));
 
 %% discretization 
 [y_dash, Dr_dash] = chebdif(N, 2);  % y_dash in [-1, 1]
 r = (h*y_dash + b + a)/2;           % radial coordinate
-rn = r/h;
-Dr1 =  2*h0/h*Dr_dash(:,:,1);       % differentiation on [-1/2, 1/2]
-Dr2 = (2*h0/h)^2*Dr_dash(:,:,2);    % 2nd order differentiation on [-1/2, 1/2]
+rn = r/h;                           % uses thickness h as normalization thickness
+Dr1 =  2*Dr_dash(:,:,1);            % differentiation on unit domain
+Dr2 = (2)^2*Dr_dash(:,:,2);         % 2nd order differentiation on unit domain
 Id = eye(size(Dr1));                % identity matrix for discretization
-rn1inv = diag(1./rn);               % 1/r^2 (mostly for use in BC)
+rn1inv = diag(1./rn);               % necessary for BC
 
 %% problem setup: (i*kh)^2*L2 + (i*kh)*L1(i*n) + L0(i*n) + w^2*M = 0
 L2 = kron(cxx, Id); 
-L1 = kron(Cxp*A, diag(1./rn)) + kron(Crx, Dr1) + 1i*n*kron(Cxp, diag(1./rn));
-L0 = kron(crr, Dr2) + kron(Crp*A, diag(1./rn)*Dr1 - diag(1./rn.^2)) ...
-    + kron(cpp, diag(1./rn)*Dr1) - kron(cpp*B, diag(1./rn.^2)) ...
-    + 1i*n*(kron(Crp, diag(1./rn)*Dr1 - diag(1./rn.^2)) + kron(2*cpp*A, diag(1./rn.^2))) ...
-    + (1i*n).^2*(kron(cpp, diag(1./rn.^2)));
-M = kron(rho, Id);
+L1 = kron(cxr + crx, Dr1) + kron(crx + cxp*(1i*n*I + A) + (1i*n*I + A)*cpx, diag(1./rn));
+L0 =   kron( crr, Dr2 ) ...
+     + kron( crr + crp*(1i*n*I + A) + (1i*n*I + A)*cpr, diag(1./rn)*Dr1 ) ...
+     + kron( (1i*n*I + A)*cpp*(1i*n*I + A), diag(1./rn.^2) ); 
+M = kron(rhon*I, Id);
 
 % incorporate BCs:
-B1 = kron(crx, Id([1, N], :)); % BC going into L1
-B0 = kron(crr, Dr1([1, N], :)) + kron(crp*A, rn1inv([1, N], :)) + 1i*n*kron(crp, rn1inv([1, N], :)); % BC going into L0
-dofBC = [(0:length(udof)-1)*N+1; (1:length(udof))*N]; % [1, N, N+1, 2*N, 2*N+1, 3*N];
+B1 = kron(crx,  Id([1, N], :)); % BC going into L1
+B0 = kron(crr, Dr1([1, N], :)) + kron(crp*(1i*n*I + A), rn1inv([1, N], :)); % into L0
+dofBC = [1, N, N+1, 2*N, 2*N+1, 3*N]; % equations to replace by BCs
 L2(dofBC, :) = 0; L1(dofBC, :) = B1; L0(dofBC, :) = B0; M(dofBC, :) = 0;
 
 %% solve for frequency:
-tic 
-whn = nan(size(M, 2), length(kh));
-for ii = 1:length(kh)
-    kh0 = kh(ii);
-    [wh2] = polyeig((1i*kh0)^2*L2 + (1i*kh0)*L1 + L0, M); % does not work properly with eig()
-    whn(:,ii) = sqrt(wh2);
+whn = nan(length(k),size(M, 2)); tic,
+khn = k*h; % re-scale to computational units (use h as unit distance)
+for ii = 1:length(khn)
+    [wh2] = eig(khn(ii)^2*L2 - 1i*khn(ii)*L1 - L0, M, 'vector');
+    whn(ii,:) = sort(sqrt(wh2));
 end
-fh = real(whn/2/pi*f0*h0); fh(fh == 0) = nan;
-chron = toc;
-fprintf('nF: %d, nK: %d, elapsed time: %g, time per point: %g. ms\n', size(fh, 2), size(fh, 1), chron, chron/length(fh(:))*1e3);
+w = real(whn)*fh0/h; % convert back to SI units
+w(w == 0) = nan;  % remove zero eigenvalues due to singular matrix M
+chron = toc; fprintf('nF: %d, nK: %d, elapsed time: %g, time per point: %g. ms\n', size(w, 2), size(w, 1), chron, chron/length(w(:))*1e3);
 
-% wavenumbers:
-kkh = kh.*ones(size(fh));
-figure, plot(kkh(:), fh(:), '.');
-ylim([0, 6e3]),
-ylabel('fh in m/s'), xlabel('kh in 1')
+% % reference to test against: 
+mat = MaterialIsotropic('noname', lbd, mu, rho);
+guide = Cylinder(mat, [a, b], N);
+gew = guide.fullyCoupled(n); 
+dat = computeW(gew,k); 
 
-% phase vel:
-kkh = kh.*ones(size(fh));
-figure, plot(fh(:), 2*pi*fh(:)./kkh(:), '.');
-ylim([0, 15e3]), xlim([0, 6e3])
-xlabel('fh in m/s'), ylabel('cp in m/s')
-
-%% solve for wavenumbers:
-% tic
-% kh = nan(length(fh), size(M, 2)*2);
-% for ii = 1:length(fh)
-%     whn = 2*pi*fh(ii)/f0/h0; % current frequency-thickness (normalized)
-%     [un, khi] = polyeig(L0 + whn^2*M, L1, L2); % solve eigenvalue problem
-%     kh(ii, :) = -1i*khi; % extract kh
-% end
-% chron = toc;
-% fprintf('nF: %d, nK: %d, elapsed time: %g, time per point: %g. ms\n', size(kh, 1), size(kh, 2), chron, chron/length(kh(:))*1e3);
-% 
-% % wave numbers:
-% ffh = fh.*ones(size(kh));
-% figure, scatter(real(kh(:)), ffh(:), 8, abs(imag(kh(:))), 'filled'), 
-% caxis([0, 0.12]), ylim([0, fh(end)]), xlim([0, 14]),
-% xlabel('kh in 1'), ylabel('fh in m/s')
-% 
-% % phase vel:
-% ffh = fh.*ones(size(kh));
-% figure, scatter(ffh(:), 2*pi*ffh(:)./real(kh(:)), 8, abs(imag(kh(:))), 'filled'), 
-% caxis([0, 0.12]), xlim([0, fh(end)]), ylim([0, 15e3]),
-% ylabel('cp in m/s'), xlabel('fh in m/s')
-
-%% compare with reference implementation:
-% plate = Plate(mat, h, 30);
-% Nmodes = 80;
-% freq = linspace(1e-2, 6, 300).'*1e6;
-% data = plate.solveForFreq(freq, Nmodes);
-% dr = data.selectModes(real(data.k) >= 0); 
-% ff0 = freq.*ones(size(dr.k));
-% plot(real(dr.k(:))*h, ff0(:)*h, 'rx'), hold on
-% % scatter(real(dr.k(:)*h), ff(:)*h, 10, abs(imag(dr.k(:)*h)), 'filled'), caxis([0, 0.12])
-% plot(kkh(:), fh(:), 'k.');
-% ylim([0, 6e3]), xlim([0, kh(end)])
-% ylabel('fh in m/s'), xlabel('kh in 1')
-% legend({'plate', 'cylinder'}, 'Location', 'south east')
+% plot:
+figure(1); clf; hold on;
+phref = plot(dat.k, dat.w/2/pi, 'x', 'Color', 0.7*[1 1 1]); 
+kk = k.*ones(size(w)); % expand to same size
+phSCM = plot(kk, w/2/pi,  'k');
+ylim([0, 6e3/h]);
+legend([phSCM(1), phref(1)], {'SCM', 'GEWtool'}, 'Location', 'southeast')
+ylabel('frequency in Hz'), xlabel('wavenumber in rad/m')
