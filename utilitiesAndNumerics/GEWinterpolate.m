0a1774ea5033b2b47fcd12f00b8961ce60849353
Daniel Kiefer <daniel.kiefer@fau.de>
2024-06-06 15:10:24 +0200

    GEWdat/GEWinterpolate: now additionally supports an array for argument 'u', instead of a cell array.

--- utilitiesAndNumerics/GEWinterpolate.m
+++ utilitiesAndNumerics/GEWinterpolate.m
@@ -2,28 +2,45 @@ function [ui, yi] = GEWinterpolate(gew, u, yi)
 % GEWinterpolate - Interpolate the data over all layers of the waveguide.
 % 
 % Arguments: 
-% - gew:     (Waveguide object) Description of the waveguide geometry and
-%            discretization
-% - u:       (cell array) Data of _one_ selected mode to be interpolated,
-%            e.g., ''dat.u(1,1,:,:)'' returned by the solvers.
-% - yi:      - scalar: numer of coordinates that will be generated on an
-%              equidistant grid on [gew.geom.yItf(1), gew.geom.yItf(end)].
-%            - vector: the coordinates on which to interpolate (should be 
-%              in the domain of the waveguide cross-section.
+% - gew:      (Waveguide object) Description of the waveguide.
+% - u:        (cell array of numeric arrays | numeric array) Data of _one_
+%             selected mode to be interpolated, e.g., ''dat.u{1}(1,1,:,:)'' returned 
+%             by computeW() or computeK(). Use extractModes() to extract the data of 
+%             one mode of a mulit-layer waveguide.
+% - Ni | yi:  - Ni (scalar): numer of coordinates that will be generated on an
+%               equidistant grid on [gew.geom.yItf(1), gew.geom.yItf(end)].
+%             - yi (vector): the coordinates on which to interpolate (should be 
+%               in the domain of the provided data (i.e. the waveguide cross-section).
+% 
+% Return values: 
+% - ui:    (array) data interpolated to coordinates yi
+% - yi:    (vector) coordinates yi
 % 
 % When ''gew'' is a symmetrized geometry, i.e., y in [0, ymax], GEWinterpolate
 % will retrieve field data for yi < 0 by symmetrical extension. You just need to
 % provide the negative coordinates. When providing the number of interpolation
 % points instead of coordinates, GEWinterpolate will automatically interpolate
 % on the symmetric domain [-gew.geom.yItf(end), gew.geom.yItf(end)].
+% 
+% Example: 
+% >> dat = computeW(gew,k);         % compute solutions for vector of wavenumber 'k'
+% >> indk = 5;                      % fifth wavenumber
+% >> indw = 6;                      % sixth frequency
+% >> datMode = extractModes(dat,indk,indw); % data of only mode (indk, indw)
+% >> [ui, yi] = GEWinterpolate(gew, datMode.u, 200); % interpolate onto 200 equi-distant coordinates yi
 %
 % Relies on the barycentric Lagrange interpolation implemented by Greg von
 % Winckel in ''barylag''.
 %
-% See also: barylag
+% See also: extractModes, barylag
 %
 % 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
+if ~iscell(u) && gew.geom.nLay ~= 1  % for multilayered waveguide we need a cell array for u
+    error('GEWTOOL:GEWinterpolate:incorrectDataStructure', 'The waveguide has multiple layers but the data structure is not a cell array.');
+elseif ~iscell(u) && gew.geom.nLay == 1
+    uu{1} = u; u = uu; % wrap into cell for consistency
+end
 if isscalar(yi) && isreal(yi) && mod(yi,1) == 0 % test if integer real scalar
     Ni = yi; % the number of nodes was provided rather than the interpolation grid.
     if gew.geom.symmetrized 
@@ -34,7 +51,7 @@ if isscalar(yi) && isreal(yi) && mod(yi,1) == 0 % test if integer real scalar
 end
 
 for l = 1:gew.geom.nLay
-    u{l} = squeeze(u{l});  % remove singleton dimension remaining from selecting a mode
+    u{l} = squeeze(u{l});  % extract layer data and squeeze
     N = gew.geom.N(l);     % expected number of nodes 
     Ndata = size(u{l},1);  % actual number of nodes
     if N ~= Ndata 
