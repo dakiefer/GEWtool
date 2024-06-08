479c79d9783bfdabe1de6131b4e7bd23141d3738
Daniel Kiefer <daniel.kiefer@fau.de>
2024-06-08 16:43:35 +0200

    GEWdat/energyVelMag() added to compute the magnitude of the energy velocity vectors.

---
+++ GEWdat/energyVelMag.m
@@ -0,0 +1,19 @@
+function [ceMag] = energyVelMag(gew, dat)
+% energyVelMag - Energy velocity magnitude |ce|.
+% 
+% The energy velocity vector ce is the radio of the total power flux vector P to the 
+% total stored energy H: ce = P/H . energyVelMag() returns the magnitude of ce.
+% 
+% See also: energyVel, energyVelAxial, energyVelTransverse, powerFlux
+% 
+% 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+cex = energyVelAxial(gew, dat);      % ex-direction
+cez = energyVelTransverse(gew, dat); % ez-direction
+ceMag = sqrt(cex.^2 + cez.^2);
+
+% NOTE: ^ You should do the above computation in terms of energy velocity
+% components instead of power flux components. The former are better scaled
+% (closer to unity) than the latter.
+
+end
