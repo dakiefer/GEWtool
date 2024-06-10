641caa703b1f6b285035dba2ca1459d5b810aea6
Daniel Kiefer <daniel.kiefer@fau.de>
2024-06-10 11:39:16 +0200

    GEWdat: rename energyVel to energyVelVec.

--- GEWdat/energyVelVec.m
+++ GEWdat/energyVelVec.m
@@ -1,4 +1,4 @@
-function [ce] = energyVel(gew, dat)
+function [ce] = energyVelVec(gew, dat)
 % ENERGYVEL - Energy velocity vector ce.
 % The energy velocity vector ce is the radio of the total power flux vector P to the 
 % total stored energy H: ce = P/H . 
