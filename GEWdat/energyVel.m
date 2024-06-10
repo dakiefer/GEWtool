8b22c1d0e255f3da85886e80dcccab05cd7243a6
Daniel Kiefer <daniel.kiefer@fau.de>
2024-06-10 11:39:50 +0200

    GEWdat: energyVel() is now alias to energyVelMag().

---
+++ GEWdat/energyVel.m
@@ -0,0 +1,10 @@
+function [ceMag] = energyVel(gew, dat)
+% ENERGYVEL - Alias to energyVelMag(). Energy velocity magnitude |ce|.
+% 
+% See also: energyVelMag, energyVelVec
+% 
+% 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+ceMag = energyVelMag(gew, dat);
+
+end
