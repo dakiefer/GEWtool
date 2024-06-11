1665f0a1b0d3836c9d20b72b00efa1274b95487f
Daniel Kiefer <daniel.kiefer@fau.de>
2024-06-11 17:55:53 +0200

    GEWintegrate(): correct integration in cylindrical coordinates

--- utilitiesAndNumerics/GEWintegrate.m
+++ utilitiesAndNumerics/GEWintegrate.m
@@ -15,18 +15,43 @@ function [I] = GEWintegrate(gew, f, n)
 %
 % 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
-if isa(gew,"Cylinder")
-    warning('Cylinders do not support this function yet. The results will be wrong.');
-end
 if nargin < 3 
     n = 3; % dimension to be integrated;
 end
 s = size(f{1}); s(n) = []; % remove dimension to be integrated
 I = zeros(s);
-for i = 1:gew.geom.nLay  % for every layer
-    hi = gew.geom.hl(i); % thickness of layer i
-    w = shiftdim(gew.lay(i).w(:), -n+1);       % integration weights moved to dim n
-    I = I + reshape( sum(w.*f{i}*hi, n) , s ); % reshape removes the singleton dimension
+for l = 1:gew.geom.nLay  % for every layer
+    lay = gew.lay(l);
+    hi = gew.geom.hl(l); % thickness of layer i
+    w = shiftdim(lay.w(:), -n+1);       % integration weights moved to dim n
+    if isa(gew,"Cylinder") && lay.r(1) ~= 0
+        r = shiftdim(lay.r(:), -n+1); % shifted to dimension of integration
+        integrand = f{l}.*r;
+    elseif isa(gew,"Cylinder") && lay.r(1) == 0 % avoid integrating at r = 0
+        [flNew, ri, wi] = interpolateToGaussLegendre(lay, f{l}, n);
+        w = wi; % replace integration weights with those of the new grid
+        integrand = flNew.*ri;
+    else
+        integrand = f{l};
+    end
+    I = I + reshape( sum(w.*integrand*hi, n) , s ); % reshape removes the singleton dimension
 end
 
+end
+
+function [flNew, ri, wi] = interpolateToGaussLegendre(lay, fl, n)
+    dimsFl = ndims(fl);
+    [yi, wi] = lgwt(lay.N,0,1); % Gauss-Legendre points and weights
+    yi = flip(yi); % upside-down
+    wi = shiftdim(wi, -n+1);      % replace by integration weights on integration nodes yi
+    % interpolate each component of f onto new coordinates yi:
+    fl = shiftdim(fl,(n-1)); % integration on first dimension
+    fNorm = norm(fl,'fro');
+    comps = reshape(fl, lay.N, [])/fNorm; % all components of the data
+    for k = 1:size(comps,2)
+        data = [lay.eta(:), comps(:,k)];
+        comps(:,k) = barylag(data,yi); % replace with interpolated data
+    end
+    flNew = shiftdim(reshape(comps, size(fl)), dimsFl-n+1)*fNorm; % positive shift is performed circularly while negative ones add singleton dimensions
+    ri = shiftdim(yi*lay.h, -n+1);    % scale to SI units, inner radius is zero
 end
