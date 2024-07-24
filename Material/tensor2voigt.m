7e294a135b41410b6903a6ca808742180a52cad9
Daniel Kiefer <daniel.kiefer@fau.de>
2024-07-24 17:48:45 +0200

    support for 3rd-order tensors in tensor2voigt() and voigt2tensor().

--- Material/tensor2voigt.m
+++ Material/tensor2voigt.m
@@ -1,20 +1,52 @@
-function C = tensor2voigt(c)
-% TENSOR2VOIGT - returns the 6x6 Voigt notated stiffness corresponding to the 
-% 4th order stiffness tensor c.
+function M = tensor2voigt(t)
+% TENSOR2VOIGT - returns the 6x6 or 3x6 Voigt notated matrix corresponding to the 
+% 4th or 3rd order tensor t.
 % 
-% see also: D. Royer and T. Valier-Brasier, Ondes élastiques dans les solides 
-% % (Elastic waves in solids), vol. 1, 2 vols. London: ISTE éditions, 2021.
+% See also: D. Royer and T. Valier-Brasier, Ondes élastiques dans les solides 
+% (Elastic waves in solids), vol. 1, 2 vols. London: ISTE éditions, 2021.
 %
-% 2022 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+% 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
-validateattributes(c,{'numeric'},{'size',[3 3 3 3]});
-C = zeros(6, 6);
-ij = [{1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
-for I = 1:6
-    for J = I:6 % start from I (symmetry in C) 
-        C(I, J) = c(ij{I,:}, ij{J, :});
-        C(J, I) = c(ij{I,:}, ij{J, :});
+    ss = size(t); % size of matrix 
+    if ndims(t) == 4 & all(ss == [3 3 3 3])
+        M = tensor2voigt4(t);
+    elseif ndims(t) == 3 & all(ss == [3 3 3])
+        M = tensor2voigt3(t);
+    else
+        error('GEWTOOL:tensor2voigt:wrongarg','Only 4th and 3rd order tensors are supported. Provide an array of size 3x3x3x3 or 3x3x3.');
     end
-end
 
 end % function 
+
+
+function C = tensor2voigt4(c)
+    % tensor2voigt4 - convert 4th-order tensors
+    %
+    % 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+    validateattributes(c,{'numeric'},{'size',[3 3 3 3]});
+    C = zeros(6, 6);
+    ij = [{1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
+    for I = 1:6
+        for J = I:6 % start from I (symmetry in C) 
+            C(I, J) = c(ij{I,:}, ij{J, :});
+            C(J, I) = c(ij{I,:}, ij{J, :});
+        end
+    end
+end % function 
+
+
+function E = tensor2voigt3(e)
+    % tensor2voigt4 - convert 4th-order tensors
+    %
+    % 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+    E = zeros(3, 6);
+    jk = [ {1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
+    % assign E_i{J} from e_i{jk}, where {jk} in {11,22,33,23,32,13,31,12,21} -> J in {1..6} :
+    for i = 1:3
+        for J = 1:6
+            E(i,J) = e(i,jk{J,:});
+        end
+    end
+end % function
--- Material/voigt2tensor.m
+++ Material/voigt2tensor.m
@@ -1,30 +1,67 @@
-function c = voigt2tensor(C)
-% VOIGT2TENSOR returns the 4th order stiffness tensor c corresponding to the Voigt
-% notated matrix C.
+function t = voigt2tensor(M)
+% VOIGT2TENSOR returns the tensor t corresponding to the Voigt notated matrix M.
+% This function can handle 4th and 3rd order tensors, which correspond to 6x6 or 3x6
+% Voigt-notated matrices, respectively.
 % 
-% see also: D. Royer and T. Valier-Brasier, Ondes élastiques dans les solides 
+% See also: D. Royer and T. Valier-Brasier, Ondes élastiques dans les solides 
 % (Elastic waves in solids), vol. 1, 2 vols. London: ISTE éditions, 2021.
 %
-% 2022 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+% 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
-validateattributes(C,{'numeric'},{'size',[6 6]});
-if ~issymmetric(C), error('Voigt matrix C must be symmetric.'); end
-c = zeros(3, 3, 3, 3);
-ij = [ {1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
-ji = [ {1},{1}; {2},{2}; {3},{3}; {3},{2}; {1},{3}; {2},{1}]; % voigt -> tensor indices
-for I = 1:6
-    for J = I:6 % start from I (symmetry in C) -> explicitly assign major symmetry below
-        % minor symmetries: 
-        c(ij{I,:}, ij{J,:}) = C(I, J); % cijkl
-        c(ij{I,:}, ji{J,:}) = C(I, J); % cijlk
-        c(ji{I,:}, ij{J,:}) = C(I, J); % cjikl
-        c(ji{I,:}, ji{J,:}) = C(I, J); % cjilk
-        % major symmetry:
-        c(ij{J,:}, ij{I,:}) = C(I, J); % cklij
-        c(ij{J,:}, ji{I,:}) = C(I, J); % cklji
-        c(ji{J,:}, ij{I,:}) = C(I, J); % clkij
-        c(ji{J,:}, ji{I,:}) = C(I, J); % clkji
+    ss = size(M); % size of matrix 
+    if all(ss == [6 6])
+        t = voigt2tensor4(M);
+    elseif all(ss == [3 6])
+        t = voigt2tensor3(M);
+    else
+        error('GEWTOOL:voigt2tensor:wrongarg','Voigt matrix must be of size 6x6 or 3x6');
     end
-end
 
+end % function
+
+
+% % convert 4th-order tensors
+function c = voigt2tensor4(C)
+    % voigt2tensor4 - convert 4th-order tensors
+    %
+    % 2022-2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+    validateattributes(C,{'numeric'},{'size',[6 6]});
+    if ~issymmetric(C), error('Voigt matrix C must be symmetric.'); end
+    c = zeros(3, 3, 3, 3);
+    ij = [ {1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
+    ji = [ {1},{1}; {2},{2}; {3},{3}; {3},{2}; {1},{3}; {2},{1}]; % voigt -> tensor indices
+    for I = 1:6
+        for J = I:6 % start from I (symmetry in C) -> explicitly assign major symmetry below
+            % minor symmetries: 
+            c(ij{I,:}, ij{J,:}) = C(I, J); % cijkl
+            c(ij{I,:}, ji{J,:}) = C(I, J); % cijlk
+            c(ji{I,:}, ij{J,:}) = C(I, J); % cjikl
+            c(ji{I,:}, ji{J,:}) = C(I, J); % cjilk
+            % major symmetry:
+            c(ij{J,:}, ij{I,:}) = C(I, J); % cklij
+            c(ij{J,:}, ji{I,:}) = C(I, J); % cklji
+            c(ji{J,:}, ij{I,:}) = C(I, J); % clkij
+            c(ji{J,:}, ji{I,:}) = C(I, J); % clkji
+        end
+    end
+end % function
+
+
+function e = voigt2tensor3(E)
+    % voigt2tensor3 - convert 3rd-order tensors
+    %
+    % 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
+
+    validateattributes(E,{'numeric'},{'size',[3 6]});
+    e = zeros(3, 3, 3);
+    jk = [ {1},{1}; {2},{2}; {3},{3}; {2},{3}; {3},{1}; {1},{2}]; % voigt -> tensor indices
+    kj = [ {1},{1}; {2},{2}; {3},{3}; {3},{2}; {1},{3}; {2},{1}]; % voigt -> tensor indices
+    % assign e_i{jk} from E_i{J}, where J in {1..6} -> {jk} in {11,22,33,23,32,13,31,12,21} :
+    for i = 1:3
+        for J = 1:6
+            e(i, jk{J,:}) = E(i, J);
+            e(i, kj{J,:}) = E(i, J);
+        end
+    end
 end % function
