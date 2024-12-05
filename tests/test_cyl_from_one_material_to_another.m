bdfe09334e49efbfc3a4690ee67113d55040e981
Daniel Kiefer <daniel.kiefer@fau.de>
2024-12-05 18:29:58 +0100

    tests: avoid warning due to new computeW()

--- tests/test_cyl_from_one_material_to_another.m
+++ tests/test_cyl_from_one_material_to_another.m
@@ -20,7 +20,7 @@ k = linspace(1e-2, 5, 50)/(r(end)-r(1)); % wavenumber-thickness (solve for frequ
 % % only first material
 cyl = Cylinder(mats{1}, [r(1), r(3)], N(1));
 gew = cyl.fullyCoupled(n);
-dat = computeW(gew, k); 
+dat = computeW(gew, k, 20); 
 fig = figure; 
 ph1 = plot(dat.k(:), dat.w(:)/2/pi, 'o', 'MarkerEdgeColor', [.7, .7, .7], 'MarkerFaceColor', [.7, .7, .7]); 
 xlim([0, 5e3]), ylim([0, 2e6]) 
@@ -29,7 +29,7 @@ xlabel('wavenumber k in rad/m'), ylabel('frequency f in Hz')
 % % only second material
 cyl = Cylinder(mats{2}, [r(1), r(3)], N(2));
 gew = cyl.fullyCoupled(n);
-dat = computeW(gew, k); 
+dat = computeW(gew, k, 20); 
 hold on, ph2 = plot(dat.k(:), dat.w(:)/2/pi, 'x', 'MarkerEdgeColor', [.5, .5, .5], 'MarkerFaceColor', [.5, .5, .5]); drawnow;
 
 % % bilayer problem thick-thin:
@@ -39,7 +39,7 @@ for ii = 1:length(b)
     r0 = [r(1), b(ii), r(3)];
     cyl = Cylinder(mats, r0, N);
     gew = cyl.fullyCoupled(n);
-    dat = computeW(gew, k); 
+    dat = computeW(gew, k, 20); 
     ph3 = plot(dat.k, dat.w/2/pi, '-', 'Color', cc(ii,:)); drawnow;
 end 
 legend([ph1(1), ph2(1), ph3(1)], {'zircaloy', 'steel', 'layered transition'}, 'Location','southeast')
