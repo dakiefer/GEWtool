aedb6855df5b232675fe18209192c0bbca5b6a95
Daniel Kiefer <daniel.kiefer@fau.de>
2025-03-16 16:33:20 +0100

    correct notation in GEWextrapolateCircumference

--- utilities/GEWextrapolateCircumference.m
+++ utilities/GEWextrapolateCircumference.m
@@ -1,4 +1,4 @@
-function [ue, phi] = GEWextrapolateCircumference(u, n, Ntheta)
+function [ue, phi] = GEWextrapolateCircumference(u, n, Nphi)
 % GEWextrapolateCircumference - Extrapolate data along the angular direction.
 % 
 % The modal field of waves in a cylinder are 
@@ -20,13 +20,13 @@ function [ue, phi] = GEWextrapolateCircumference(u, n, Ntheta)
 %           computeK(). GEWinterpolate() directly generates data 'u' in the
 %           appropriate structure.
 % - n:      (scalar) circumferential order (as prescribed when choosing the wave family)
-% - Ntheta: (scalar, default: 60) number of points along the circumference.
+% - Nphi:   (scalar, default: 60) number of points along the circumference.
 % 
 % Return values: 
 % - ue:     (array) extrapolated field data. It has one more dimension than 'u' that
-%           describes the angular dependence. The new dimension is the second one.
-%           dimension (the first one is usually the radial dependance).
-% - theta:  (vector) vector of angular coordinates on which the extrapolation was
+%           describes the angular dependence. The new dimension is the second one (the 
+%           first one is usually the radial dependance).
+% - phi:    (vector) vector of angular coordinates on which the extrapolation was
 %           performed.
 %
 % See also: GEWinterpolate, extractModes.
@@ -34,7 +34,7 @@ function [ue, phi] = GEWextrapolateCircumference(u, n, Ntheta)
 % 2024 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
 if nargin < 3
-    Ntheta = 60;    % default 
+    Nphi = 60;    % default 
 end
 if iscell(u)        % does not support cell array
     error('GEWTOOL:GEWextrapolateCircumference', ...
@@ -48,7 +48,7 @@ else
 end
 perm = [1, nd+1, 2:nd];          % to create a new dimension at second position
 u = permute(u,perm);
-phi = linspace(0, 2*pi, Ntheta); % angular coordinates
+phi = linspace(0, 2*pi, Nphi);   % angular coordinates
 ue = u.*exp(1i*n*phi);           % extrapolate using circumf. order 'n'
 
 end
