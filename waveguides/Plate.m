4ac40dc0a76104fa30f936329b6def1c40093888
Daniel Kiefer <daniel.kiefer@fau.de>
2025-07-10 18:09:18 +0200

    BUGFIX Plate() did not work when one thickness was provided for several layers

--- waveguides/Plate.m
+++ waveguides/Plate.m
@@ -3,43 +3,68 @@ function obj = Plate(mats, zs, Ns)
 % Arguments: 
 % - mats:  materials [1 x Nlay], either of class "Material" or a struct
 %          needs to support mats.rho (scalar) and mats.c (3x3x3x3).
-% - zs:    coordinates of interfaces in meter [1 x Nlay+1]
-%          instead, if zs is of size [1 x Nlay]: zs are interpreted as the 
-%          thickness of each layer.
+% - zs:    thickness of each layer in meter [1 x Nlay] or,
+%          coordinates of interfaces in meter [1 x Nlay+1]
+%          -> infinite thickness or coordinate is interpreted as a half-space.
 % - Ns:    discretization order for each layer [1 x Nlay]
 % 
-% Example:
+% Example one layer:
 % mat = Material('steel'); % load material data
 % h = 1e-3; % thickness in m
 % N = 20; % discretization (number of nodal points)
 % plate = Plate(mat, h, N); % create waveguide description
 % 
+% Example two layers:
+% mat1 = Material('steel'); % load material data
+% mat2 = Material('pmma'); 
+% h = 1e-3; % thickness in m
+% N = 20; % discretization (number of nodal points)
+% plate = Plate({mat1 mat2}, h, N); % two layers of same thickness h
+%
+% Example leaky plate:
+% steel = Material('steel'); % load material data
+% water = Material('water'); 
+% h = 1e-3; % thickness in m
+% N = 20; % discretization (number of nodal points)
+% plate = Plate({steel water}, [h inf], N); % steel plate with water on top
+% 
+% 2025 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
 
-% extract exterior half-spaces and crop materials "mats" and coordinates "zs"
-if ~iscell(mats)
-    mats = num2cell(mats);
-end
-halfSpaces = PlateLeaky.parseLoading(mats,zs);
-mats = mats(~isinf(zs)); zs = zs(~isinf(zs)); % crop to finite layers
-
-% standard specification of materials "mats" and coordinates "zs"
-if isscalar(zs) && length(mats) > 1 % use same thickness for all layers
-    zs = zs*ones(size(mats)); % expand into a vector of thicknesses
-end
-if length(mats) == length(zs) % thicknesses have been provided
-    if isscalar(zs)
-        zs = zs*[-1/2, 1/2]; % single layer has centered coordinate 
-    else
-        zs = [0, cumsum(zs)]; % coordinates of interfaces starting from 0
+    % convert material list to a cell array 
+    if ~iscell(mats)
+        mats = num2cell(mats);
     end
-elseif length(zs) ~= length(mats)+1
-    error('GEWTOOL:Plate:wrongArguments','Provide either a thickness for each layer or the coordinates of the interfaces.');
-end
+    
+    % convert to list of thicknesses "hs" for each layer (if not yet the case):
+    if isscalar(zs) && length(mats) > 1 % one thickness provided for multiple layers
+        zs = zs*ones(size(mats)); % use same thickness for all layers
+    end
+    if length(zs) == length(mats) + 1 % coordinates of interfaces provided
+        zs = diff(zs); % convert to thickness for each layer
+    end
+    hs = zs; % here we will always have a list of thicknesses for each mats{i}
 
-if isempty(halfSpaces)
-    obj = PlateClosed(mats, zs, Ns); 
-else
-    obj = PlateLeaky(mats, zs, Ns, halfSpaces); 
-end
+    % extract half-spaces and crop materials "mats" and coordinates "zs"
+    halfSpaces = PlateLeaky.parseLoading(mats,hs);
+    mats = mats(~isinf(hs)); hs = hs(~isinf(hs)); % crop to finite layers
+    
+    % convert to coordinates "zs" for all layers of finite thickness:
+    if isscalar(hs)
+        zs = hs*[-1/2, 1/2]; % single layer has centered coordinate 
+    else
+        zs = [0, cumsum(hs)]; % coordinates of interfaces starting from 0
+    end
+    
+    % error checking:
+    if length(zs) ~= length(mats)+1
+        error('GEWTOOL:Plate:wrongArguments','Provide either a thickness for each layer or the coordinates of the interfaces.');
+    end
+    
+    % create "PlateClosed" or "PlateLeaky" object:
+    if isempty(halfSpaces) % no exterior half-spaces
+        obj = PlateClosed(mats, zs, Ns); % closed plate (no radiation)
+    else
+        obj = PlateLeaky(mats, zs, Ns, halfSpaces); 
+    end
 
 end
