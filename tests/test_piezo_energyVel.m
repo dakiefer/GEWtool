72d079abb5767e5cf477050c569ff23f9e6ce038
Daniel Kiefer <daniel.kiefer@fau.de>
2026-02-23 18:23:05 +0100

    test_piezo_energyVel: equipartition of energy is tested.

--- tests/test_piezo_energyVel.m
+++ tests/test_piezo_energyVel.m
@@ -1,7 +1,11 @@
 % Test that the energy velocity is correctly computed in piezoelectric waveguides. 
 % 
-% This compares the axial energy velocity cex to the axial group velocity cgx. A
-% comparison to dw/dk is also done. 
+% This compares the axial energy velocity cex to the axial group velocity cgx.
+% A comparison to dw/dk is also done. 
+% 
+% Only electrically closed plate (fixed potential) is tested. The open plate has
+% an electrical field outside the plate. Energy balance over the plate alone is,
+% hence, not sufficient. 
 %
 % Run using: runtests(). see also: 
 % https://fr.mathworks.com/help/matlab/matlab_prog/write-script-based-unit-tests.html
@@ -48,7 +52,7 @@ cex = energyVelAxial(dat);
 dev = abs(cgx - cex)/mat.cl;
 if exist('show', 'var') && show
     maxDev = max(dev,[],'all')
-    figure(3); clf; hold on; title('AlN - Lamb')
+    figure(3); clf; hold on; title('AlN - fully coupled')
     plot(dat.w(:), cgx(:), 'rx', 'DisplayName','cg');
     plot(dat.w(:), cex(:), 'k.', 'DisplayName','ce');
     legend('Location','southeast');
@@ -92,9 +96,30 @@ cex = energyVelAxial(dat);
 dev = abs(cgx - cex)/mat.cl;
 if exist('show', 'var') && show
     maxDev = max(dev,[],'all')
-    figure(3); clf; hold on; title('fully coupled')
+    figure(3); clf; hold on; title('LNB - fully coupled')
     plot(dat.w(:), cgx(:), 'rx', 'DisplayName','cg');
     plot(dat.w(:), cex(:), 'k.', 'DisplayName','ce');
+    legend('Location','southeast');
+end
+assert( max(dev,[],'all') <= tol )
+
+%% equipartition of energy
+mat = MaterialPiezoelectric('lithium_niobate');
+mat = mat.rotateEuler(-pi/7,'x'); % some strange orientation
+guide = Plate(mat, h, N);
+gew = guide.fullyCoupled; 
+gew = gew.fixGdof(gew.geom.gdofBC{1}(4,:)); % fix potential at top and bottom -> closed boundary conditions! 
+dat = computeW(gew, k, 10);
+
+Ekin = energyKinetic(dat); 
+Ecomp = energyElastic(dat) + energyElectric(dat); 
+
+dev = abs( (Ekin(:)-Ecomp(:))/norm(Ekin(:)) );
+if exist('show', 'var') && show
+    maxDev = max(dev,[],'all')
+    figure(3); clf; hold on; title('equipartition of energy')
+    plot(dat.w(:), Ekin(:), 'rx', 'DisplayName','kin');
+    plot(dat.w(:), Ecomp(:), 'k.', 'DisplayName','ela + elec');
     legend('Location','southeast');
 end
 assert( max(dev,[],'all') <= tol )
