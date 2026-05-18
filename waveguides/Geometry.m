6aa714d8340b7be107601b65149b0f83bb9dbfd8
Daniel Kiefer <daniel.kiefer@fau.de>
2026-05-18 12:01:52 +0200

    Geometry: refactored to use dependent properties calculated on the fly for consistency.

--- waveguides/Geometry.m
+++ waveguides/Geometry.m
@@ -10,26 +10,26 @@ classdef Geometry
 % 2022 - Daniel A. Kiefer, Institut Langevin, ESPCI Paris, France
     
 properties 
-    nLay         % number of layers
-    nItf         % number of interfaces, includes outer ones
     N            % number of nodal points for each layer
     Nudof        % number of degrees of freedom (displacement components, elec. potential, etc...)
     zItf         % position of interfaces in meter [nLay x 2]
     z            % cell array with nodal points for each layer in meter
-    hl           % thickness for each layer in meter
     % nodesOfElem  % connectivity map: row e contains left and right node num of elem e -> TODO not being used
-    ldofBC       % cell array of local dofs at boundaries: e.g. [1, N; N+1, 2*N] -> [upper, lower]
     gdofDBC      % fixed degrees of freedom
-    gdofOfLay    % cell array with global dofs assigned to layers
-    Ndof         % total number of degrees of freedom
     symmetrized = false  % true/false: whether the geometry represents the full or symmetry part
 end
 
 properties (Dependent)
-    gdofBC      % a link to all gdofOfLay(ldofBC) assembled into one array: size [ui, 2 (bottom/top)]
-    gdofFree    % degrees of freedom where no Dirichlet-BCs are imposed
-    gdofRedX    % degrees of freedom for the x-displacements in the reduced matrices (due to Dirichlet BC)
-    gdofRedZ    % degrees of freedom for the z-displacements in the reduced matrices (due to Dirichlet BC)
+    nLay         % number of layers
+    nItf         % number of interfaces, includes outer ones
+    hl           % thickness for each layer in meter
+    Ndof         % total number of degrees of freedom
+    gdofOfLay    % cell array with global dofs assigned to layers
+    ldofBC       % cell array of local dofs at boundaries: e.g. [1, N; N+1, 2*N] -> [upper, lower]
+    gdofBC       % a link to all gdofOfLay(ldofBC) assembled into one array: size [ui, 2 (bottom/top)]
+    gdofFree     % degrees of freedom where no Dirichlet-BCs are imposed
+    gdofRedX     % degrees of freedom for the x-displacements in the reduced matrices (due to Dirichlet BC)
+    gdofRedZ     % degrees of freedom for the z-displacements in the reduced matrices (due to Dirichlet BC)
 end
 
 methods
@@ -45,46 +45,70 @@ methods
         %          (optional, default per layer: 3)
 
         % mesh parameters:
-        if nargin < 3, Nudof = 3*ones(size(N)); end
+        if nargin < 3, Nudof = []; end
         if ~isvector(zItf) % nLay x 2 given
             if size(zItf, 2)~=2
                 error('Incorrect size of interface coordinates. Expected a vector or nx2 array.');
             end
             zItf = [zItf(:,1).', zItf(end,2)]; % reduce to vector nLay+1 x 1
         end
-        obj.nLay = length(zItf) - 1;
+        obj.zItf = [zItf(1:end-1).', zItf(2:end).'];
         obj.N = N(:); % discretization order: number of collocation points
         obj.Nudof = Nudof(:); % how many of the ux, uy, uz degrees of freedom
-        obj.nItf = length(zItf);
-        obj.zItf = [zItf(1:end-1).', zItf(2:end).'];
-        obj.hl = diff(obj.zItf, 1, 2); % first order along 2nd dim
+        for e=1:obj.nLay % length(N) == number of layers
+            etad = Layer.nodes(obj.N(e)); 
+            obj.z{e} = etad*obj.hl(e) + obj.zItf(e, 1);
+        end
         % mesh connectivity:
         % obj.nodesOfElem = zeros(obj.nLay, 2); % connectivity map
         % obj.nodesOfElem(:,1) = 1:obj.nItf-1;
         % obj.nodesOfElem(:,2) = 2:obj.nItf;
+    end
+
+    function nLay = get.nLay(obj)
+        nLay = size(obj.zItf,1);
+    end
+
+    function nItf  = get.nItf(obj)
+        nItf = size(obj.zItf,1)+1;
+    end
+
+    function hl = get.hl(obj)
+        hl = diff(obj.zItf, 1, 2); % first order along 2nd dim
+    end
+
+    function Ndof = get.Ndof(obj)
+        Ndof = numel(unique([obj.gdofOfLay{:}]));
+    end
+
+    function gdofOfLay = get.gdofOfLay(obj)
         % boundary condition nodes:
-        for e=1:length(N) % length(N) == number of layers
-            obj.ldofBC{e} = (0:Nudof(e)-1).'*N(e) + [1, N(e)];
-            etad = Layer.nodes(N(e)); 
-            obj.z{e} = etad*obj.hl(e) + obj.zItf(e, 1);
+        gdofOfLay = cell(obj.nLay,1);
+        for e=1:obj.nLay
             if e == 1
-                gdofE = 1:Nudof(e)*N(e);
+                gdofE = 1:obj.Nudof(e)*obj.N(e);
             else
-                Ncommon = min(Nudof(e-1:e)); % number of common degrees of freedom at interface
-                gdofBCLast = obj.gdofOfLay{e-1}(obj.ldofBC{e-1}); % global dofs at boundary of last layer
+                Ncommon = min(obj.Nudof(e-1:e)); % number of common degrees of freedom at interface
+                gdofBCLast = gdofOfLay{e-1}(obj.ldofBC{e-1}); % global dofs at boundary of last layer
                 gdofCommon = gdofBCLast(end-Ncommon+1:end,2).'; % dofs of previous layer at interface
-                gdofLastLay = obj.gdofOfLay{e-1}(end);
-                Nnew = Nudof(e)*N(e)-Ncommon; % subtract common number of dofs with previous layer
+                gdofLastLay = gdofOfLay{e-1}(end);
+                Nnew = obj.Nudof(e)*obj.N(e)-Ncommon; % subtract common number of dofs with previous layer
                 gdofNew = gdofLastLay + (1:Nnew); 
-                ldofInd = 1:Nudof(e)*N(e);
+                ldofInd = 1:obj.Nudof(e)*obj.N(e);
                 ldofCommon = obj.ldofBC{e}(1:Ncommon);
                 ldofNew = setdiff(ldofInd, ldofCommon);
                 gdofE = zeros(1,length(ldofInd));
                 gdofE(ldofNew) = gdofNew; gdofE(ldofCommon) = gdofCommon;
             end
-            obj.gdofOfLay{e} = gdofE;
+            gdofOfLay{e} = gdofE;
         end
-        obj.Ndof = numel(unique([obj.gdofOfLay{:}]));
+    end
+
+    function ldofBC = get.ldofBC(obj)
+        ldofBC = cell(obj.nLay,1);
+        for e=1:obj.nLay
+            ldofBC{e} = (0:obj.Nudof(e)-1).'*obj.N(e) + [1, obj.N(e)];
+        end
     end
 
     function gdofBC = get.gdofBC(obj)
